4a4ec8745cafb4f1727c9f17deaafa903ad0c707
Artem <31816473+ofantomas@users.noreply.github.com>
2019-04-25 22:56:02 +0300

    fix bugs and add improvements
    add trained model and test it

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import os
 import tqdm
 
 
@@ -10,6 +11,7 @@ class BLSTM(torch.nn.Module):
         self.device = device
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        self.num_classes = num_classes
         self.embedding = torch.nn.Embedding.from_pretrained(embeddings)
         self.lstm = torch.nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, 
                                   dropout=dropout, bidirectional=True).to(self.device)
@@ -31,19 +33,20 @@ class BLSTM(torch.nn.Module):
         return out
     
 class nnPredictor():
-    def __init__(self, input_size, hidden_size, num_layers, num_classes, embeddings, dropout=0.5, 
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, embeddings, batch_size=256, dropout=0.5, 
                  learning_rate=1e-3, criterion=torch.nn.BCEWithLogitsLoss, optimizer=torch.optim.Adam, load_from=None):
         self.device = torch.device('cuda' if torch.cuda.is_available() is True else 'cpu')
         self.learning_rate = learning_rate
         self.model = BLSTM(input_size, hidden_size, num_layers, num_classes, embeddings, self.device, dropout)
         self.criterion = criterion(reduction='mean')
         self.optimizer = optimizer
+        self.batch_size = batch_size
         
         
         if load_from is not None:
             self.model.load_state_dict(torch.load(os.getcwd() + "\\" + load_from))
         
-    def train(self, data_train, data_test=None, num_epochs=3, batch_size=64, scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau):
+    def train(self, data_train, data_test=None, num_epochs=3, scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau):
         self.optim = self.optimizer(self.model.parameters(), lr=self.learning_rate)
         scheduler = scheduler(self.optim, 'min', patience=2)
         
@@ -51,7 +54,7 @@ class nnPredictor():
         test_loss = []
          
         for epoch in range(num_epochs):
-            with tqdm.tqdm(enumerate(data_train.batch_generator()), total=int(len(data_train) / batch_size)) as iterator:
+            with tqdm.tqdm(enumerate(data_train.batch_generator()), total=int(len(data_train) / self.batch_size)) as iterator:
                 for batch_num, batch in iterator:
                     self.optim.zero_grad()
                     text, labels = batch
@@ -68,19 +71,19 @@ class nnPredictor():
             
             if data_test is not None:
                 self.model.eval()
-                val_loss = self.test(data_test, batch_size)
+                val_loss = self.test(data_test)
                 scheduler.step(val_loss)
                 test_loss.append(val_loss)
                 self.model.train()
         
         return train_loss, test_loss
 
-    def test(self, data_test, batch_size=64):
+    def test(self, data_test):
         #add other reductions + add functionality to remove predict
         scores = []
     
         with torch.no_grad():
-            with tqdm.tqdm(enumerate(data_test.batch_generator()), total=int(len(data_test) / batch_size)) as iterator:
+            with tqdm.tqdm(enumerate(data_test.batch_generator()), total=int(len(data_test) / self.batch_size)) as iterator:
                 for batch_num, batch in iterator:
                     text, labels = batch
                     labels = labels.to(self.device)
@@ -95,15 +98,18 @@ class nnPredictor():
         torch.save(self.model.state_dict(), path)
         
     def predict(self, dataloader):
+        res = []
+        
+        
         with torch.no_grad():
-            res = []
-            for batch in dataloader.batch_generator():
-                output = torch.sigmoid(self.model(batch)).to(torch.device('cpu'))
-                res.append(output)
+            with tqdm.tqdm(enumerate(dataloader.batch_generator()), total=int(len(dataloader) / self.batch_size)) as iterator:
+                for batch_num, batch in iterator:
+                    output = torch.sigmoid(self.model(batch)).to(torch.device('cpu'))
+                    res.append(output)
         
         last = res[-1]
         res = torch.stack(res[:-1])
-        res = res.reshape(-1, 6)
+        res = res.reshape(-1, self.model.num_classes)
         res = torch.cat((res, last), 0)
         
         return res
